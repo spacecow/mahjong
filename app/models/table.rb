590560be3bb11cbe21e1e0edfeba362b0b7fdbe9
Johan Sveholm <jsveholm@gmail.com>
2011-03-08 15:07:58 +0900

    reservation can be saved without user id

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,5 +1,5 @@
 class Table < ActiveRecord::Base
   has_many :reservations, :dependent => :destroy
   has_many :users, :through => :reservations
-  accepts_nested_attributes_for :reservations, :reject_if => lambda {|a| a[:user_id].blank?}, :allow_destroy => true
+  accepts_nested_attributes_for :reservations
 end
